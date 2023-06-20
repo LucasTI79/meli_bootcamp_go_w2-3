5e84ec5ae7c709c138bb290a38f99a1fcc7fffce
Josias Barreto <josias.borges@mercadolivre.com>
2023-06-20 08:49:25 -0300

    delete file .DS_store

--- cmd/server/routes/routes.go
+++ cmd/server/routes/routes.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"database/sql"
 
-	docs "github.com/extmatperez/meli_bootcamp_go_w2-3/cmd/server/docs"
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/cmd/server/handler"
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/buyer"
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/employee"
@@ -14,6 +13,7 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/swag/example/basic/docs"
 )
 
 type Router interface {
