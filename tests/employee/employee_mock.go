62de1bb4a6e77e99ed7081228b9bf7ab32ea1248
vihvasc <vihvasc@gmail.com>
2023-06-25 16:03:07 -0300

    add all mocks

--- tests/employee/employee_mock.go
+++ tests/employee/employee_mock.go
@@ -24,3 +24,48 @@ func (m *EmployeeRepositoryMock) GetAll(ctx context.Context) ([]domain.Employee,
 	args := m.Called(ctx)
 	return args.Get(0).([]domain.Employee), args.Error(1)
 }
+
+func (m *EmployeeServiceMock) Save(ctx context.Context, e domain.Employee) (domain.Employee, error) {
+	args := m.Called(ctx, e)
+	return args.Get(0).(domain.Employee), args.Error(1)
+}
+
+func (m *EmployeeRepositoryMock) Save(ctx context.Context, e domain.Employee) (int, error) {
+	args := m.Called(ctx, e)
+	return args.Get(0).(int), args.Error(1)
+}
+
+func (m *EmployeeServiceMock) Delete(ctx context.Context, id int) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
+
+func (m *EmployeeRepositoryMock) Delete(ctx context.Context, id int) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
+
+func (m *EmployeeServiceMock) Get(ctx context.Context, id int) (domain.Employee, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(domain.Employee), args.Error(1)
+}
+
+func (m *EmployeeRepositoryMock) Get(ctx context.Context, id int) (domain.Employee, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(domain.Employee), args.Error(1)
+}
+
+func (m *EmployeeServiceMock) Update(ctx context.Context, e domain.Employee, id int) (domain.Employee, error) {
+	args := m.Called(ctx, e, id)
+	return args.Get(0).(domain.Employee), args.Error(1)
+}
+
+func (m *EmployeeRepositoryMock) Update(ctx context.Context, e domain.Employee) error {
+	args := m.Called(ctx, e)
+	return args.Error(0)
+}
+
+func (m *EmployeeRepositoryMock) Exists(ctx context.Context, warehouseCode string) bool {
+	args := m.Called(ctx, warehouseCode)
+	return args.Get(0).(bool)
+}
