dab85033c9386fd63b8103d58b19d5c405399e1f
vihvasc <vihvasc@gmail.com>
2023-06-25 15:47:31 -0300

    add tests getall and mock

--- cmd/server/handler/employee_test.go
+++ cmd/server/handler/employee_test.go
@@ -1,1 +1,52 @@
 package handler_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
+	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/employee"
+	mocks "github.com/extmatperez/meli_bootcamp_go_w2-3/tests/employee"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	GetAllEmployees = "/employees"
+)
+
+func TestGetAllEmployees(t *testing.T) {
+	t.Run("Should return all employeess when repository is called", func(t *testing.T) {
+		expectedEmployees := []domain.Employee{
+			{
+				ID:           01,
+				CardNumberID: "001",
+				FirstName:    "Joana",
+				LastName:     "Silva",
+				WarehouseID:  1,
+			},
+			{
+				ID:           02,
+				CardNumberID: "002",
+				FirstName:    "Beatriz",
+				LastName:     "Costa",
+				WarehouseID:  1,
+			},
+		}
+
+		repository, service := InitServerWithEmployeesRepository(t)
+		repository.On("GetAll", mock.Anything).Return(expectedEmployees, nil)
+
+		employees, err := service.GetAll(context.TODO())
+
+		assert.True(t, len(employees) == 2)
+		assert.NoError(t, err)
+	})
+}
+
+func InitServerWithEmployeesRepository(t *testing.T) (*mocks.EmployeeRepositoryMock, employee.Service) {
+	t.Helper()
+	mockRepository := &mocks.EmployeeRepositoryMock{}
+	mockService := employee.NewService(mockRepository)
+	return mockRepository, mockService
+}
--- tests/employee/employee_mock.go
+++ tests/employee/employee_mock.go
@@ -1,1 +1,21 @@
 package mocks
+
+import (
+	"context"
+
+	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type EmployeeServiceMock struct {
+	mock.Mock
+}
+
+type EmployeeRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *EmployeeServiceMock) GetAll(ctx context.Context) ([]domain.Employee, error) {
+	args := m.Called(ctx)
+	return args.Get(0).([]domain.Employee), args.Error(1)
+}
