e9d3bb94b10009808ab9d41057a60a41894f28db
Quezia Fonseca de Lima <qfonseca1@gmail.com>
2023-06-25 19:46:57 -0300

    tests: add get product by id (service) tet

--- internal/product/service_test.go
+++ internal/product/service_test.go
@@ -2,6 +2,7 @@ package product_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
@@ -11,7 +12,7 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-func TestGetAll(t *testing.T) {
+func TestGetAllProducts(t *testing.T) {
 	t.Run("Should return all products when repository is called", func(t *testing.T) {
 		expectedProducts := []domain.Product{
 			{
@@ -51,6 +52,43 @@ func TestGetAll(t *testing.T) {
 
 		assert.True(t, len(products) == 2)
 		assert.NoError(t, err)
+	})
+}
+
+func TestGetProductsById(t *testing.T) {
+	t.Run("Should return the product when it exists", func(t *testing.T) {
+		expectedProduct := domain.Product{
+
+			ID:             1,
+			Description:    "milk",
+			ExpirationRate: 1,
+			FreezingRate:   2,
+			Height:         6.4,
+			Length:         4.5,
+			Netweight:      3.4,
+			ProductCode:    "PROD02",
+			RecomFreezTemp: 1.3,
+			Width:          1.2,
+			ProductTypeID:  1,
+			SellerID:       1,
+		}
+
+		service, repository := CreareProductService(t)
+
+		repository.On("Get", mock.Anything).Return(expectedProduct, nil)
+
+		product, err := service.Get(context.TODO(), 1)
+
+		assert.Equal(t, expectedProduct, product)
+		assert.NoError(t, err)
+	})
+	t.Run("Should return an error when the product does not exists", func(t *testing.T) {
+		service, repository := CreareProductService(t)
+		expectedError := errors.New("product not found")
+		repository.On("Get", mock.Anything, mock.Anything).Return(domain.Product{}, product.ErrNotFound)
+		_, err := service.Get(context.TODO(), 1)
+		assert.Equal(t, expectedError, err)
+		assert.Error(t, err)
 	})
 }
 
