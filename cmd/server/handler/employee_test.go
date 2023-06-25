3da63c4ec22e93a4e6f006f7868ef7dc33cc5436
vihvasc <vihvasc@gmail.com>
2023-06-25 17:38:05 -0300

    add create tests

--- cmd/server/handler/employee_test.go
+++ cmd/server/handler/employee_test.go
@@ -2,6 +2,7 @@ package handler_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
@@ -49,4 +50,55 @@ func InitServerWithEmployeesRepository(t *testing.T) (*mocks.EmployeeRepositoryM
 	mockRepository := &mocks.EmployeeRepositoryMock{}
 	mockService := employee.NewService(mockRepository)
 	return mockRepository, mockService
+}
+
+func TestCreateEmployees(t *testing.T) {
+	t.Run("Should create the employee if it contains the required fields", func(t *testing.T) {
+		id := 4
+		expectedEmployee := domain.Employee{
+			CardNumberID: "001",
+			FirstName:    "Joana",
+			LastName:     "Silva",
+			WarehouseID:  1,
+		}
+
+		repository, service := InitServerWithEmployeesRepository(t)
+		repository.On("Exists", mock.Anything, "001").Return(false)
+		repository.On("Save", mock.Anything, expectedEmployee).Return(id, nil)
+
+		employee, err := service.Save(context.TODO(), expectedEmployee)
+
+		assert.Equal(t, "Joana", employee.FirstName)
+		assert.Equal(t, "Silva", employee.LastName)
+		assert.Equal(t, "001", employee.CardNumberID)
+		assert.Equal(t, 1, employee.WarehouseID)
+		assert.Equal(t, 4, employee.ID)
+
+		assert.NoError(t, err)
+	})
+	t.Run("Should return err employee already exists when employee already exists", func(t *testing.T) {
+		expectedMessage := "employee already exists"
+
+		repository, service := InitServerWithEmployeesRepository(t)
+
+		repository.On("Exists", mock.Anything, mock.Anything).Return(true)
+
+		_, err := service.Save(context.TODO(), domain.Employee{})
+
+		assert.Error(t, err)
+		assert.Equal(t, expectedMessage, err.Error())
+	})
+	t.Run("Should return error when there is an save repository error", func(t *testing.T) {
+		repository, service := InitServerWithEmployeesRepository(t)
+
+		repository.On("Exists", mock.Anything, mock.Anything).Return(false)
+
+		expectedError := errors.New("some error")
+		repository.On("Save", mock.Anything, domain.Employee{}).Return(0, expectedError)
+
+		_, err := service.Save(context.TODO(), domain.Employee{})
+
+		assert.Error(t, err)
+		assert.Equal(t, expectedError, err)
+	})
 }
