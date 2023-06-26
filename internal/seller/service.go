284269d150df77151ad4ca81e37c3d7292f3bd90
Gabrieli Padilha <gabrielip.souza@hotmail.com>
2023-06-26 09:02:12 -0300

    refactor seller service

--- internal/seller/service.go
+++ internal/seller/service.go
@@ -12,7 +12,6 @@ var (
 	ErrInvalidId    = errors.New("invalid id")
 	ErrInvalidBody  = errors.New("invalid body")
 	ErrTryAgain     = errors.New("error, try again %s")
-	ErrAlredyExists = errors.New("seller already exists")
 	ErrCidAlreadyExists = errors.New("cid already registered")
 	ErrSaveSeller       = errors.New("error saving seller")
 )
@@ -22,7 +21,7 @@ type Service interface {
 	Get(ctx context.Context, id int) (domain.Seller, error)
 	Save(ctx context.Context, d domain.Seller) (domain.Seller, error)
 	Delete(ctx context.Context, id int) error
-	Update(ctx context.Context, s domain.Seller) error
+	Update(ctx context.Context, id int, s domain.Seller) (domain.Seller, error)
 }
 
 type sellerService struct {
@@ -38,7 +37,7 @@ func NewService(r Repository) Service {
 func (s *sellerService) GetAll(ctx context.Context) ([]domain.Seller, error) {
 	sellers, err := s.repository.GetAll(ctx)
 	if err != nil {
-		return []domain.Seller{}, err
+		return []domain.Seller{}, ErrTryAgain
 	}
 	return sellers, nil
 }
@@ -65,10 +64,33 @@ func (s *sellerService) Get(ctx context.Context, id int) (domain.Seller, error) 
 	return seller, err
 }
 
-func (s *sellerService) Update(ctx context.Context, d domain.Seller) error {
-	if !s.repository.Exists(ctx, d.CID) {
-		return errors.New("cannot modify CID")
+func (s *sellerService) Update(ctx context.Context, id int, newSeller domain.Seller) (domain.Seller, error) {
+	seller, err := s.repository.Get(ctx, id)
+	if err != nil {
+		return domain.Seller{}, ErrNotFound
 	}
-	err := s.repository.Update(ctx, d)
-	return err
+	if newSeller.CID != 0 {
+		if newSeller.CID != seller.CID {
+			cidAlreadyExists := s.repository.Exists(ctx, newSeller.CID)
+			if cidAlreadyExists {
+				return domain.Seller{}, ErrCidAlreadyExists
+			}
+		}
+		seller.CID = newSeller.CID
+	}
+	if newSeller.Address != "" {
+		seller.Address = newSeller.Address
+	}
+	if newSeller.CompanyName != "" {
+		seller.CompanyName = newSeller.CompanyName
+	}
+	if newSeller.Telephone != "" {
+		seller.Telephone = newSeller.Telephone
+	}
+
+	errUpdate := s.repository.Update(ctx, seller)
+	if errUpdate != nil {
+		return domain.Seller{}, errUpdate
+	}
+	return seller, nil
 }
