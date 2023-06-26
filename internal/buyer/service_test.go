6ed5acde7a48e2046f266163677cd7ec2cd7c5c8
giuliago <giu_goliveira@hotmail.com>
2023-06-26 06:21:28 -0300

    Revert "tests: service delete cases"
    This reverts commit 4b3940ce260e1378dd482ac39497b83ccfc54016.

--- internal/buyer/service_test.go
+++ internal/buyer/service_test.go
@@ -7,6 +7,10 @@ comprador será devolvido com as informações
 atualizadas
 
 UPDATE update_non_existent Se o comprador a ser atualizado não existir, será retornado null.
+
+DELETE delete_non_existent Quando o comprador não existir, será devolvido null.
+
+DELETE delete_ok Se a exclusão for bem-sucedida, o item não aparecerá na lista.
 */
 import (
 	"context"
@@ -97,35 +101,6 @@ func TestGetById(t *testing.T) {
 		repository.On("Get", mock.Anything, mock.Anything).Return(domain.Buyer{}, buyer.ErrNotFound)
 
 		_, err := service.Get(context.TODO(), 1)
-
-		assert.Error(t, err)
-		assert.Equal(t, expectedError, err)
-	})
-}
-
-func TestDeleteWarehouses(t *testing.T) {
-	t.Run("Should delete the buyer when it exists", func(t *testing.T) {
-		expectedBuyer := domain.Buyer{
-			ID:           9,
-			CardNumberID: "2556",
-			FirstName:    "Giulianna",
-			LastName:     "Oliveira",
-		}
-
-		repository, service := InitServerWithBuyersRepository(t)
-		repository.On("Delete", mock.Anything, expectedBuyer.ID).Return(nil)
-
-		err := service.Delete(context.TODO(), 9)
-
-		assert.NoError(t, err)
-	})
-	t.Run("Should return nill when buyer dont exists", func(t *testing.T) {
-		repository, service := InitServerWithBuyersRepository(t)
-
-		expectedError := errors.New("buyer not found")
-		repository.On("Delete", mock.Anything, mock.Anything).Return(buyer.ErrNotFound)
-
-		err := service.Delete(context.TODO(), 19)
 
 		assert.Error(t, err)
 		assert.Equal(t, expectedError, err)
