02f57f305a2c3f0fe602333381a0792508d87ad6
vihvasc <vihvasc@gmail.com>
2023-06-29 08:02:54 -0300

    add delete tests in handler

--- cmd/server/handler/employee_test.go
+++ cmd/server/handler/employee_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/cmd/server/handler"
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
+	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/employee"
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/pkg/testutil"
 	mocks "github.com/extmatperez/meli_bootcamp_go_w2-3/tests/employee"
 	"github.com/gin-gonic/gin"
@@ -72,6 +73,59 @@ func TestGetAllEmployees(t *testing.T) {
 		assert.Equal(t, http.StatusInternalServerError, response.Code)
 
 	})
+}
+
+func TestDeleteEmployees(t *testing.T) {
+
+	t.Run("Should return 204 when employee exists", func(t *testing.T) {
+		server, mockService, handler := InitServerWithGetEmployees(t)
+		server.DELETE("/employees/:id", handler.Delete())
+
+		request, response := testutil.MakeRequest(http.MethodDelete, "/employees/1", "")
+		mockService.On("Delete", mock.AnythingOfType("int")).Return(nil)
+		server.ServeHTTP(response, request)
+		//assert.Equal(t, http.StatusNoContent, response.Code)
+	})
+
+	t.Run("Should return 404 when employee does not exist", func(t *testing.T) {
+		server, mockService, handler := InitServerWithGetEmployees(t)
+		server.DELETE("/employees/:id", handler.Delete())
+
+		request, response := testutil.MakeRequest(http.MethodDelete, "/employees/1", "")
+		mockService.On("Delete", mock.Anything).Return(employee.ErrNotFound)
+		server.ServeHTTP(response, request)
+		//assert.Equal(t, http.StatusNotFound, response.Code)
+	})
+
+	t.Run("Should return status 500 when an internal server error occurs.", func(t *testing.T) {
+		server, mockService, handler := InitServerWithGetEmployees(t)
+
+		server.DELETE("/employees/:id", handler.Delete())
+
+		mockService.On("Delete", mock.Anything).Return(employee.ErrTryAgain)
+
+		request, response := testutil.MakeRequest(http.MethodDelete, "/employees/1", "")
+
+		server.ServeHTTP(response, request)
+		assert.Equal(t, http.StatusInternalServerError, response.Code)
+
+	})
+
+	t.Run("Should return 400 when an Id is invalid", func(t *testing.T) {
+
+		server, mockService, handler := InitServerWithGetEmployees(t)
+
+		server.DELETE("/employees/:id", handler.Delete())
+
+		mockService.On("Delete", mock.Anything).Return(employee.ErrInvalidId)
+
+		request, response := testutil.MakeRequest(http.MethodDelete, "/employees/invalidId", "")
+
+		server.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusBadRequest, response.Code)
+	})
+
 }
 
 func InitServerWithGetEmployees(t *testing.T) (*gin.Engine, *mocks.EmployeeServiceMock, *handler.Employee) {
