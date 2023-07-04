a72c7d77c8a67f82e3c4508332ebc404a961e584
giuliago <giu_goliveira@hotmail.com>
2023-07-04 17:23:35 -0300

    repository methods

--- internal/purchase_orders/repository.go
+++ internal/purchase_orders/repository.go
@@ -1,19 +1,19 @@
 package purchase_orders
 
 import (
-	//"context"
+	"context"
 	"database/sql"
-	//"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
+
+	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
 )
 
 // Repository encapsulates the storage of a purchased order.
 type Repository interface {
-	//GetAll(ctx context.Context) ([]domain.Buyer, error)
-	//Get(ctx context.Context, id int) (domain.Buyer, error)
-	//Exists(ctx context.Context, cardNumberID string) bool
-	//Save(ctx context.Context, b domain.Buyer) (int, error)
-	//Update(ctx context.Context, b domain.Buyer) error
-	//Delete(ctx context.Context, id int) error
+	GetAll(ctx context.Context) ([]domain.PurchaseOrders, error)
+	Get(ctx context.Context, id int) (domain.PurchaseOrders, error)
+	ExistsOrder(ctx context.Context, orderNumber string) bool
+	ExistsBuyer(ctx context.Context, cardNumberID string) bool
+	Save(ctx context.Context, o domain.PurchaseOrders) (int, error)
 }
 
 type repository struct {
@@ -24,4 +24,26 @@ func NewRepository(db *sql.DB) Repository {
 	return &repository{
 		db: db,
 	}
+}
+
+func (r *repository) GetAll(ctx context.Context) ([]domain.PurchaseOrders, error) {
+	var purchaseorders []domain.PurchaseOrders
+	return purchaseorders, nil
+}
+
+func (r *repository) Get(ctx context.Context, id int) (domain.PurchaseOrders, error) {
+	var purchaseorders domain.PurchaseOrders
+	return purchaseorders, nil
+}
+
+func (r *repository) ExistsOrder(ctx context.Context, orderNumber string) bool {
+	return false
+}
+
+func (r *repository) ExistsBuyer(ctx context.Context, cardNumberID string) bool {
+	return false
+}
+
+func (r *repository) Save(ctx context.Context, o domain.PurchaseOrders) (int, error) {
+	return 0, nil
 }
