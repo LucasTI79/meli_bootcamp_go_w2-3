296795492f3f7db5258dd8e7f679b6ca90ba8b43
Alexsandro Xavier <alexsandro.xaviers@mercadolivre.com>
2023-07-04 12:39:19 -0300

    feat: created querys for save, verify if product and section exists and products by section

--- internal/product_batch/repository.go
+++ internal/product_batch/repository.go
@@ -1,1 +1,8 @@
 package productbatch
+
+const (
+	SaveQuery         = "INSERT INTO product_batches ( batch_number, current_quantity, current_temperature, due_date, initial_quantity, manufacturing_date, manufacturing_hour, minimum_temperature, product_id, section_id) VALUES (?,?,?,?,?,?,?,?,?,?)"
+	ProductExists     = "SELECT id FROM products WHERE id=?"
+	SectionExists     = "SELECT id FROM sections WHERE id=?"
+	ProductsBySection = "SELECT count(pb.id) as `products_count`,pb.section_id, s.section_number FROM product_batches pb JOIN section ON pb.section_id = s.id GROUP BY pb.section_id"
+)
