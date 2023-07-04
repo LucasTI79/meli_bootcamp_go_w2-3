ccf93840987d2ce78a1949e4b01878639a053a4b
giuliago <giu_goliveira@hotmail.com>
2023-07-04 17:30:10 -0300

    handler methods

--- cmd/server/handler/purchase_orders.go
+++ cmd/server/handler/purchase_orders.go
@@ -8,7 +8,7 @@ import (
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/purchase_orders"
 	//"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
 	//"github.com/extmatperez/meli_bootcamp_go_w2-3/pkg/web"
-	//"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type PurchaseOrdersController struct {
@@ -18,5 +18,20 @@ type PurchaseOrdersController struct {
 func NewPurchaseOrders(o purchase_orders.Service) *PurchaseOrdersController {
 	return &PurchaseOrdersController{
 		purchaseordersService: o,
+	}
+}
+
+func (b *PurchaseOrdersController) Get() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func (b *PurchaseOrdersController) GetAll() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func (b *PurchaseOrdersController) Create() gin.HandlerFunc {
+	return func(c *gin.Context) {
 	}
 }
