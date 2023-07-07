b8e05ee9e097e9408b182ff10da429b17ea06905
Quezia Fonseca de Lima <qfonseca1@gmail.com>
2023-07-07 08:51:14 -0300

    feat: add  RecordByProduct struct

--- internal/domain/product.go
+++ internal/domain/product.go
@@ -37,3 +37,9 @@ type ProductResponse struct {
 type ProductResponseById struct {
 	Data Product `json:"data"`
 }
+
+type RecordByProduct struct {
+	ProductID    int    `json:"product_id"`
+	Description  string `json:"description"`
+	RecordsCount int    `json:"records_count"`
+}
