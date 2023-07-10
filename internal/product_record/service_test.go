96273fa6d1246bf040b336592d538b4e19250e28
Quezia Fonseca de Lima <qfonseca1@gmail.com>
2023-07-10 09:04:25 -0300

    test: add service test TestRecordsByOneProductReport

--- internal/product_record/service_test.go
+++ internal/product_record/service_test.go
@@ -2,6 +2,7 @@ package productrecord_test
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"testing"
 
@@ -38,6 +39,33 @@ func TestRecordsByAllProductsReport(t *testing.T) {
 	})
 }
 
+func TestRecordsByOneProductReport(t *testing.T) {
+	t.Run("Should return the product record when it exists", func(t *testing.T) {
+
+		expectedReport := domain.ProductRecordReport{
+			ProductID:    1,
+			Description:  "Product 1",
+			RecordsCount: 3,
+		}
+		service, repository := CreateProductRecordService(t)
+
+		repository.On("RecordsByOneProductReport", mock.Anything).Return(expectedReport, nil)
+
+		report, err := service.RecordsByOneProductReport(context.TODO(), 1)
+
+		assert.Equal(t, expectedReport, report)
+		assert.NoError(t, err)
+	})
+	t.Run("Should return an error when the product does not exists", func(t *testing.T) {
+		expectedEmpityReport := domain.ProductRecordReport{}
+		service, repository := CreateProductRecordService(t)
+		expectedError := errors.New("product not found")
+		repository.On("RecordsByOneProductReport", mock.Anything).Return(expectedEmpityReport, productrecord.ErrNotFound)
+		_, err := service.RecordsByOneProductReport(context.TODO(), 1)
+		assert.Equal(t, expectedError, err)
+		assert.Error(t, err)
+	})
+}
 func CreateProductRecordService(t *testing.T) (productrecord.Service, *mocks.ProductRecordRepositoryMock) {
 	mockRepository := new(mocks.ProductRecordRepositoryMock)
 	mockService := productrecord.NewService(mockRepository)
