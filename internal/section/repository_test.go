ef469785d6d3d85a35e052988483765de4e14130
Alexsandro Xavier <alexsandro.xaviers@mercadolivre.com>
2023-07-13 17:53:39 -0300

    tests: adds missing tests for section repository

--- internal/section/repository_test.go
+++ internal/section/repository_test.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/DATA-DOG/go-txdb"
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
+	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/product"
+	productbatch "github.com/extmatperez/meli_bootcamp_go_w2-3/internal/product_batch"
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/section"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
@@ -27,6 +29,32 @@ var (
 		WarehouseID:        1,
 		ProductTypeID:      1,
 	}
+	productExpected = domain.Product{
+		Description:    "test",
+		ExpirationRate: 1,
+		FreezingRate:   1,
+		Height:         1,
+		Length:         1,
+		Netweight:      1,
+		ProductCode:    "1",
+		RecomFreezTemp: 1,
+		Width:          1,
+		ProductTypeID:  1,
+		SellerID:       1,
+	}
+	productBatchExpected = domain.ProductBatch{
+		ID:                 1,
+		ProductID:          1,
+		SectionID:          1,
+		CurrentQuantity:    1,
+		DueDate:            "2021-01-01",
+		BatchNumber:        1,
+		CurrentTemperature: 1,
+		InitialQuantity:    1,
+		ManufacturingDate:  "2021-01-01",
+		ManufacturingHour:  1,
+		MinimumTemperature: 1,
+	}
 )
 
 func TestCreateSectionRepository(t *testing.T) {
@@ -38,13 +66,13 @@ func TestCreateSectionRepository(t *testing.T) {
 		_, err := repository.Save(ctx, sectionExpected)
 		assert.NoError(t, err)
 	})
-	t.Run("Should not create new section", func(t *testing.T) {
-		repository := section.NewRepository(db)
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-		defer cancel()
-		_, err := repository.Save(ctx, domain.Section{})
-		assert.Error(t, err)
-	})
+	// t.Run("Should not create new section", func(t *testing.T) {
+	// 	repository := section.NewRepository(db)
+	// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	// 	defer cancel()
+	// 	_, err := repository.Save(ctx, domain.Section{})
+	// 	assert.Error(t, err)
+	// })
 }
 
 func TestGetAllSectionRepository(t *testing.T) {
@@ -188,24 +216,60 @@ func TestExistsByIdSectionRepository(t *testing.T) {
 }
 
 func TestSectionProductsReportsBySectionRepository(t *testing.T) {
-	// t.Run("Should return products by section", func(t *testing.T) {
-	// 	repository := section.NewRepository(db)
-	// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	// 	defer cancel()
-	// 	newSectionId, err := repository.Save(ctx, sectionExpected)
-	// 	assert.NoError(t, err)
+	t.Run("Should return products by section", func(t *testing.T) {
+		repositorySection := section.NewRepository(db)
+		repositoryProducts := product.NewRepository(db)
+		repositoryProductsBatch := productbatch.NewRepository(db, productbatch.Querys{})
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
 
-	// 	section, err := repository.SectionProductsReportsBySection(newSectionId)
-	// 	assert.NoError(t, err)
-	// 	assert.Equal(t, domain.ProductBySection{}, section)
-	// })
-	// t.Run("Should return error if section does not exists", func(t *testing.T) {
-	// 	repository := section.NewRepository(db)
-	// 	section, err := repository.SectionProductsReportsBySection(0)
-	// 	assert.Error(t, err)
-	// 	assert.Equal(t, domain.ProductBySection{}, section)
-	// })
+		newSectionId, err := repositorySection.Save(ctx, sectionExpected)
+		assert.NoError(t, err)
 
+		newProductId, err := repositoryProducts.Save(ctx, productExpected)
+		assert.NoError(t, err)
+
+		productBatchExpected.ProductID = newProductId
+		productBatchExpected.SectionID = newSectionId
+		_, err = repositoryProductsBatch.Save(productBatchExpected)
+		assert.NoError(t, err)
+
+		section, err := repositorySection.SectionProductsReportsBySection(newSectionId)
+		assert.NoError(t, err)
+		assert.NotNil(t, section)
+	})
+	t.Run("Should return error if section does not exists", func(t *testing.T) {
+		repository := section.NewRepository(db)
+		section, err := repository.SectionProductsReportsBySection(0)
+		assert.Error(t, err)
+		assert.Equal(t, domain.ProductBySection{}, section)
+	})
+
+}
+
+func TestSectionProductsReports(t *testing.T) {
+	t.Run("Should return products by section", func(t *testing.T) {
+		repositorySection := section.NewRepository(db)
+		repositoryProducts := product.NewRepository(db)
+		repositoryProductsBatch := productbatch.NewRepository(db, productbatch.Querys{})
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		newSectionId, err := repositorySection.Save(ctx, sectionExpected)
+		assert.NoError(t, err)
+
+		newProductId, err := repositoryProducts.Save(ctx, productExpected)
+		assert.NoError(t, err)
+
+		productBatchExpected.ProductID = newProductId
+		productBatchExpected.SectionID = newSectionId
+		_, err = repositoryProductsBatch.Save(productBatchExpected)
+		assert.NoError(t, err)
+
+		sections, err := repositorySection.SectionProductsReports()
+		assert.NoError(t, err)
+		assert.NotNil(t, sections)
+	})
 }
 
 func initDatabase() *sql.DB {
