a4866381542fc2c698432e48b26d389d0a2d5307
Quezia Fonseca de Lima <qfonseca1@gmail.com>
2023-07-12 21:54:30 -0300

    test: add repository create and delete product test

--- internal/product/repository_test.go
+++ internal/product/repository_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/DATA-DOG/go-txdb"
+	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/domain"
 	"github.com/extmatperez/meli_bootcamp_go_w2-3/internal/product"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
@@ -16,51 +17,19 @@ import (
 
 var db = InitDatabase()
 
-// var expectedProduct = domain.Product{
-// 	// ID:             1,
-// 	Description:    "milk",
-// 	ExpirationRate: 1,
-// 	FreezingRate:   2,
-// 	Height:         6.4,
-// 	Length:         4.5,
-// 	Netweight:      3.4,
-// 	ProductCode:    "PROD03",
-// 	RecomFreezTemp: 1.3,
-// 	Width:          1.2,
-// 	ProductTypeID:  1,
-// 	SellerID:       1,
-// }
-
-// expectedProducts := []domain.Product{
-// 	{
-// 		ID:             1,
-// 		Description:    "milk",
-// 		ExpirationRate: 1,
-// 		FreezingRate:   2,
-// 		Height:         6.4,
-// 		Length:         4.5,
-// 		Netweight:      3.4,
-// 		ProductCode:    "PROD01",
-// 		RecomFreezTemp: 1.3,
-// 		Width:          1.2,
-// 		ProductTypeID:  1,
-// 		SellerID:       1,
-// 	},
-// 	{
-// 		ID:             2,
-// 		Description:    "milk",
-// 		ExpirationRate: 1,
-// 		FreezingRate:   2,
-// 		Height:         6.4,
-// 		Length:         4.5,
-// 		Netweight:      3.4,
-// 		ProductCode:    "PROD02",
-// 		RecomFreezTemp: 1.3,
-// 		Width:          1.2,
-// 		ProductTypeID:  2,
-// 		SellerID:       2,
-// 	},
-// }
+var expectedProductResult = domain.Product{
+	Description:    "milk",
+	ExpirationRate: 1,
+	FreezingRate:   2,
+	Height:         6.4,
+	Length:         4.5,
+	Netweight:      3.4,
+	ProductCode:    "PROD03",
+	RecomFreezTemp: 1.3,
+	Width:          1.2,
+	ProductTypeID:  1,
+	SellerID:       1,
+}
 
 func TestProductsGetAll(t *testing.T) {
 	t.Run("Should get all products", func(t *testing.T) {
@@ -96,6 +65,55 @@ func TestProductGet(t *testing.T) {
 		_, err := repository.Get(ctx, 50000000)
 		assert.Error(t, err)
 		assert.Equal(t, expectedMessage, err.Error())
+	})
+}
+
+func TestProductSave(t *testing.T) {
+	t.Run("should create a product", func(t *testing.T) {
+		repository := product.NewRepository(db)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		// Salva um produto e espera não obter erro
+		result, err := repository.Save(ctx, expectedProductResult)
+		assert.NoError(t, err)
+
+		// Busca o produto que foi salvo e verifica o campo product_code
+		product, err := repository.Get(ctx, result)
+		assert.NoError(t, err)
+		assert.NotNil(t, product)
+		expectedProductResult.ProductCode = "PROD03"
+		assert.Equal(t, expectedProductResult.ProductCode, product.ProductCode)
+	})
+}
+
+func TestProductDelete(t *testing.T) {
+	t.Run("It should delete a product", func(t *testing.T) {
+		repository := product.NewRepository(db)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		//Deleta um produto
+		expectedProductResult.ID = 3
+		err := repository.Delete(ctx, expectedProductResult.ID)
+		assert.NoError(t, err)
+
+		//Busca o produto que foi deletado, e verifica se não o encontra,
+		_, err = repository.Get(ctx, expectedProductResult.ID)
+		assert.Error(t, err)
+		expectedErrorMessage := product.ErrNotFound.Error()
+		assert.Equal(t, expectedErrorMessage, err.Error())
+	})
+	t.Run("It should return an error when delete a product that does not exist", func(t *testing.T) {
+		repository := product.NewRepository(db)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+		defer cancel()
+
+		// Deleta um produto que não existe e espera erro not found
+		err := repository.Delete(ctx, 50000000)
+		assert.Error(t, err)
+		expectedErrorMessage := product.ErrNotFound.Error()
+		assert.Equal(t, expectedErrorMessage, err.Error())
 	})
 }
 
